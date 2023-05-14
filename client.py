4dfcbb9ecc4ac6476a33be4356a7d88d0d9a570d
mikez3 <64916212+mikez3@users.noreply.github.com>
2023-05-14 12:39:03 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,16 +1,18 @@
 import threading
 import socket
+
 nickname = input('Pick a nickname: ')
 # Create a client object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the client to the localhost and the port
 client.connect(('127.0.0.1', 59000))
 
-# Receiving messages from other clients through the server 
-def client_recieve():
+
+# Receiving messages from other clients through the server
+def client_receive():
     while True:
         try:
-            # Decode because the message is received and not sended
+            # Decode because the message here is received from another client (and not sent)
             message = client.recv(1024).decode('utf-8')
             if message == "nickname?":
                 client.send(nickname.encode('utf-8'))
@@ -21,14 +23,16 @@ def client_recieve():
             client.close()
             break
 
+
 # Send messages to other clients through the server
 def client_send():
     while True:
-        message = f'{nickname}: {input("")}' 
+        message = f'{nickname}: {input("")}'
         client.send(message.encode('utf-8'))
 
-# Create 2 threads: one for receiving messages and one for sending
-receive_thread = threading.Thread(target=client_recieve)
+
+# Create 2 threads per client: one for receiving messages and one for sending
+receive_thread = threading.Thread(target=client_receive)
 receive_thread.start()
 send_thread = threading.Thread(target=client_send)
 send_thread.start()
